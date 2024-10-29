e97c24046116df51872dd518beaf63c17aeac5e1
Javier Morales <javiermduarte70@gmail.com>
2024-10-29 10:12:55 -0400

    added reservation page link

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path("", views.home, name='home'),
+    path('', views.home, name='home'),  # Home page at the root URL
+    path('reservation/', views.reservation_page, name='reservation_page'),  # Reservation page
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html", {})
+def reservation_page(request):
+    return render(request, 'pages/ReservationPage.html')  
