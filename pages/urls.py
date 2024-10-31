98e88b64078b961fab4aad5ae471cb4abe1ce132
Javier Morales <javiermduarte70@gmail.com>
2024-10-31 16:50:01 -0400

    changed login to index

--- pages/urls.py
+++ pages/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page at the root URL
+    path('', views.login, name='login_page'),
+    path('home/', views.home, name='home'),  # Home page at the root URL
     path('reservation/', views.reservation_page, name='reservation_page'),  # Reservation page
     path('reservations/<int:id>/modify/', views.modify_reservation, name='modify_reservation'), # Customer modify reservation
-    path('login/', views.login, name='login_page'),
     path('register/', views.register, name="registration_page"),
     path('search/', views.search_reservation, name="search_page")
 ]
