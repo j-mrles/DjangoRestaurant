bfce585bd5eb3ff64eefbcdcf45510e9fb94f627
Javier Morales <javiermduarte70@gmail.com>
2024-10-31 15:42:26 -0400

    deleted old DB entry and added new User table

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+import datetime
 
-# Create your models here.
 class User(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255, default="John") 
+    lastname = models.CharField(max_length=255, default="Doe")    
+    reservationtime = models.TimeField(default=datetime.time(19, 0))  
+    reservationdate = models.DateField(default=datetime.date(2024, 11, 1)) 
+    role = models.CharField(max_length=100, default="Guest")  
+    tablenumber = models.IntegerField(default=1)  
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} - Table {self.tablenumber}"
