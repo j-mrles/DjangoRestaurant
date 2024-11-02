5a942aa274b3b7030285f9dc8882707d1b3e9287
Javier Morales <javiermduarte70@gmail.com>
2024-11-02 11:06:42 -0400

    added login functionality

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import User
-
+from django.contrib import messages
 
 def home(request):
     users = User.objects.all()
     return render(request, 'pages/home.html', {'users': users})  
 
 def login(request):
+    username = None  
+    password = None  
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if username and password:
+            try:
+                user = User.objects.get(username=username, password=password)
+                request.session['user_id'] = user.id
+                messages.success(request, "You are logged in successfully!")
+                return redirect('reservation_page')
+            except User.DoesNotExist:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Please enter both username and password")
     return render(request, "pages/LoginComponent/LoginPage.html", {})
 
 def register(request):
