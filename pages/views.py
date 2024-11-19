085b8a36b3160b470a9d1bdbc53baf0a8633141f
Javier Morales <javiermduarte70@gmail.com>
2024-11-18 22:18:31 -0500

    wip

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import Reservation
 from .models import ResUser
 from django.contrib.auth import logout, authenticate
 from django.contrib import messages
-from django.shortcuts import redirect
 from django.db.models import Q
 import random
 from datetime import datetime, timedelta
@@ -19,30 +18,28 @@ def home(request):
     return render(request, 'pages/LoginComponent/HomePage.html', {
         'loggedin' : loggedin,
         'role': role
-        })  
+    })  
 
 def login(request):
-    messages.get_messages(request).used = True                              # clear any messages in request
-    if request.method == "POST":                                            # If the request is someone clicking a form button
+    messages.get_messages(request).used = True  # Clear any messages in request
+    if request.method == "POST":  # If the request is someone clicking a form button
         # Get the form data (For login, just username and password)
         username = request.POST.get("username")
         password = request.POST.get("password")
-        if username and password:                                           # If both fields are NOT empty
-            user = authenticate(username=username , password=password)      # Authenticate user
-            if user is not None:                                            # If user is found
+        if username and password:  # If both fields are NOT empty
+            user = authenticate(username=username, password=password)  # Authenticate user
+            if user is not None:  # If user is found
                 request.session['user_id'] = user.id
-                # consider below -> remove if doesn't work
                 request.session['username'] = username
                 messages.success(request, "You are logged in successfully!")
                 return redirect('reservation_page')
-            else:                                                           # If user is NOT found
+            else:  # If user is NOT found
                 messages.error(request, "Invalid username or password")
-        else:                                                               # If one or both fields are empty
+        else:  # If one or both fields are empty
             messages.error(request, "Please enter both username and password")
     return render(request, "pages/LoginComponent/LoginPage.html")
 
 def register(request):
-    # Clear any messages in the request
     messages.get_messages(request).used = True
     if request.method == "POST":
         # Get form data
@@ -54,12 +51,12 @@ def register(request):
         password = request.POST.get('password')
         repassword = request.POST.get('repassword')
 
-        if password and repassword:                                         # If password and repassword are not blank
-            if password == repassword:                                      # If password and repassword match
+        if password and repassword:  # If password and repassword are not blank
+            if password == repassword:  # If password and repassword match
                 try:
-                    user = User.objects.create_user(username, email, password, first_name = firstname, last_name = lastname)    # Create user object with provided data and save it
+                    user = User.objects.create_user(username, email, password, first_name=firstname, last_name=lastname)
                     user.save()
-                    resuser = ResUser.objects.create(phonenumber=phonenumber, user=user)                                        # Create resuser object to store phone number and save it
+                    resuser = ResUser.objects.create(phonenumber=phonenumber, user=user)
                     resuser.save()
                     messages.success(request, "Registration successful!")
                     return redirect('login_page')
@@ -72,14 +69,14 @@ def register(request):
     return render(request, "pages/LoginComponent/RegisterPage.html", {})
 
 def reservation_page(request):
-    messages.get_messages(request).used = True                               # Clear any messages
+    messages.get_messages(request).used = True  # Clear any messages
     
     # Authenticate user for role-specific UI elements
     loggedin = 'false'
     role = 'user'
     firstname, lastname, email, phonenumber, user = None, None, None, None, None
-    if request.session.has_key('username'): # If user is logged in
-        user = User.objects.get(username=request.session.get('username')) # Get the user object from session 'username' field
+    if 'username' in request.session:  # If user is logged in
+        user = User.objects.get(username=request.session['username'])  # Get the user object from session 'username' field
         # Fill out variables from user account
         firstname = user.first_name
         lastname = user.last_name
@@ -91,6 +88,9 @@ def reservation_page(request):
             role = 'employee'
     else:
         firstname = "Guest"
+        lastname = ""
+        email = ""
+        phonenumber = ""
 
     if request.method == "POST":
         # If user isn't logged in, get personal details from form
@@ -107,7 +107,7 @@ def reservation_page(request):
                 resuser.save()
                 
             except:
-                messages.error(request, "Error occured while creating reservation")
+                messages.error(request, "Error occurred while creating reservation")
 
         date = datetime.strptime(request.POST.get('resdate'), "%Y-%m-%d")
         time = datetime.strptime(request.POST.get('restime'), "%H:%M")
@@ -118,10 +118,12 @@ def reservation_page(request):
 
     return render(request, 'pages/ReservationComponent/ReservationPage.html', {
         'firstname': firstname,
+        'lastname': lastname,
+        'email': email,
+        'phonenumber': phonenumber,
         'loggedin': loggedin,
         'role': role,
     }) 
-    
 
 def viewall_reservations(request):
     messages.get_messages(request).used = True
@@ -138,45 +140,49 @@ def modify_reservation(request):
     return render(request, "pages/ViewReservationsPage.html")
 
 def search_reservation(request):
-    # do search database stuff here :D
+    # Fetch all reservations initially
+    reservations = Reservation.objects.all()
 
-    reservations = []
+    # Extract search parameters from the GET request
+    firstname = request.GET.get('firstname', '')
+    lastname = request.GET.get('lastname', '')
+    resdate = request.GET.get('resdate', '')
+    restime = request.GET.get('restime', '')
+    tablenum = request.GET.get('tablenum', '')
 
-    if request.method == "GET":
-        first_name = request.GET.get('search-by-firstname','')
-        last_name = request.GET.get('search-by-lastname','')
-        date = request.GET.get('search-by-resdate','')
-        time = request.GET.get('search-by-restime','')
-        tablenum = request.GET.get('search-by-tablenum','')
-        # available = request.GET.get('search-by-availability','')
-        # reserved_by = request.GET.get('search-by-reservedby','')  # this is a little janky
-        
-        if first_name or last_name or date or time or tablenum:
-            reservations = Reservation.objects.all()
+    # Print the search parameters to see what is being passed
+    print(f"Search Parameters: firstname={firstname}, lastname={lastname}, resdate={resdate}, restime={restime}, tablenum={tablenum}")
 
-            if first_name:
-                reservations = reservations.filter(reservedBy__firstname__icontains=first_name)
-            if last_name:
-                reservations = reservations.filter(reservedBy__lastname__icontains=last_name)
-            if date:
-                reservations = reservations.filter(date=date)
-            if time:
-                reservations = reservations.filter(time=time)
-            if tablenum:
-                reservations = reservations.filter(tablenum=tablenum)
-            # if available:
-            # if reserved_by:
+    # Apply filters based on input
+    if firstname:
+        print(f"Filtering by firstname: {firstname}")
+        reservations = reservations.filter(reservedBy__first_name__icontains=firstname)
+    if lastname:
+        print(f"Filtering by lastname: {lastname}")
+        reservations = reservations.filter(reservedBy__last_name__icontains=lastname)
+    if resdate:
+        print(f"Filtering by resdate: {resdate}")
+        reservations = reservations.filter(date=resdate)
+    if restime:
+        print(f"Filtering by restime: {restime}")
+        reservations = reservations.filter(time=restime)
+    if tablenum:
+        print(f"Filtering by tablenum: {tablenum}")
+        reservations = reservations.filter(tablenum=tablenum)
 
-    return render(request, "pages/ReservationComponent/ReservationSearch.html", {
-        'reservations':reservations,
-        'firstname': first_name,
-        'lastname' : last_name,
-        'date' : date,
-        'time' : time,
-        'tablenum' : tablenum
+    # Print the final filtered queryset
+    print(f"Filtered reservations: {reservations}")
+
+    # Pass the results and search terms to the template
+    return render(request, "pages/ReservationComponent/ReservationSearch.html", {    
+        'reservations': reservations,
+        'firstname': firstname,
+        'lastname': lastname,
+        'resdate': resdate,
+        'restime': restime,
+        'tablenum': tablenum
     })
-
-
+    
 def custom_logout(request):
     if messages.get_messages(request):
         messages.get_messages(request).used = True
@@ -189,34 +195,26 @@ def table_statuses(request):
     tables = [
         {
             "number": i + 1,
-            # placeholder data for each table's capacity
-            "capacity": random.choice([2, 4, 6]), 
-            # can adjust weights(%) for which is more frequent, available is occuring more
-            "availability": random.choices(["Available", "Reserved"], weights=[75, 15])[0]
+            "capacity": random.choice([2, 4, 6]),  # placeholder data for each table's capacity
+            "availability": random.choices(["Available", "Reserved"], weights=[75, 15])[0]  # Random availability
         }
-        # placeholder data for a total of 10 tables
-        for i in range(10)  
+        for i in range(10)  # placeholder data for a total of 10 tables
     ]
 
     selected_table = request.GET.get('tablenumber', '')
 
     return render(request, 'pages/ReservationComponent/TableStatuses.html', {
         "tables": tables,
-        # pass selected table to the template
         "selected_table": selected_table,  
     })
 
-
 def tableAvailability(date, time):
     reservations = Reservation.objects.all()
-
     reservations.filter(date=date)
-
     tables = [True] * 15
 
     for reservation in reservations:
-
         if reservation.time < time < reservation.time + timedelta(minutes=120):
             tables[reservation.tablenum-1] = False
-    
+
     return tables
